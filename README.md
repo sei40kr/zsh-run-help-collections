28db6c70a890bc636bb38c40d8d2b1a1c95534e3
Seong Yong-ju <sei40kr@gmail.com>
2019-11-12 23:22:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# zsh-run-help-collections
+
+## Install
+
+```sh
+# Load zsh-run-help-collections
+zplugin light sei40kr/zsh-run-help-collections
+```
+
+## How it works
+
+```shell-session
+$ docker run # press M-h
+$ # this will run
+$ run-help docker-run
+
+$ go get     # press M-h
+$ # this will run
+$ go help get
+```
---
+++ zsh-run-help-collections.plugin.zsh
@@ -0,0 +1,14 @@
+# zsh-run-help-collections.plugin.zsh
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+fpath+="${0:h}/functions"
+
+unalias run-help 2>/dev/null
+autoload -Uz \
+         run-help \
+         run-help-docker \
+         run-help-gem \
+         run-help-go \
+         run-help-npm \
+         run-help-pip \
+         run-help-yarn
